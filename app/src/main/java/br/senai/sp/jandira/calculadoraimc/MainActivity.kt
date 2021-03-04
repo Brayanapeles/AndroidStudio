5da09dfdfe73c72e7f85400e05499a1e389c0c84
Brayan Nunes de Oliveira Apeles <76599304+Brayanapeles@users.noreply.github.com>
2021-03-04 09:56:55 -0300

    adcionando itens

--- app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
@@ -2,6 +2,7 @@ package br.senai.sp.jandira.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity() {
             imc.altura = edit_altura.text.toString().toDouble()
 
             text_resultado.text = imc.calcularImc().toString()
+
+            Toast.makeText(this, "Meu primeiro programa android!", Toast.LENGTH_LONG).show()
         }
 
     }
