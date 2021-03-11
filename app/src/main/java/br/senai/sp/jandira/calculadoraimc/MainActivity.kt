6df371339af7957910f471c97387b7749f6fbec4
Brayan Nunes de Oliveira Apeles <76599304+Brayanapeles@users.noreply.github.com>
2021-03-11 11:26:36 -0300

    update de 11/03

--- app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
@@ -2,7 +2,10 @@ package br.senai.sp.jandira.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.sax.StartElementListener
+import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,15 +17,42 @@ class MainActivity : AppCompatActivity() {
 
 
         button_calcular.setOnClickListener {
-
-            var imc = imc()
-            imc.peso = edit_peso.text.toString().toDouble()
-            imc.altura = edit_altura.text.toString().toDouble()
-
-            text_resultado.text = imc.calcularImc().toString()
-
-            Toast.makeText(this, "Meu primeiro programa android!", Toast.LENGTH_LONG).show()
+            if (validar()==true) {
+                calcularImc()
+            }
         }
 
+        button_limpar.setOnClickListener {
+            limpar()
+        }
+    }
+
+    fun limpar(){
+        edit_peso.text.clear()
+        edit_altura.text.clear()
+        edit_peso.requestFocus()
+    }
+
+    fun calcularImc(){
+
+        var imc = imc()
+        imc.peso = edit_peso.text.toString().toDouble()
+        imc.altura = edit_altura.text.toString().toDouble()
+
+        text_resultado.text = imc.calcularImc().toString()
+
+        Toast.makeText(this, "Meu primeiro programa android!", Toast.LENGTH_LONG).show()
+    }
+
+    fun validar() :Boolean{
+        if (edit_peso.text.isEmpty()){
+            edit_peso.error = "Preencha este campo!"
+            return false
+        }else if (edit_altura.text.isEmpty()){
+            edit_altura.error = "Preencha este campo!"
+            return false
+        }else{
+            return true
+        }
     }
 }
